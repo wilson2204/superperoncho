46dda1fe14db2bbcfc4e68b7fd53387b45f9789a
Agustin Wilson <aguswilson2015@outlook.es>
2025-12-02 16:08:24 -0300

    init

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { registerSW } from 'virtual:pwa-register'
 
+registerSW()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/superperoncho">
     <App />
   </BrowserRouter>
 )
-
-registerSW()
--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: "/superperoncho/",
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico'],
-      manifest: {
-        name: "Mi Supermercado",
-        short_name: "Super",
-        start_url: "/superperoncho/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#4caf50",
-        description: "Aplicación PWA de un supermercado",
-        icons: [
-          {
-            src: "/superperoncho/icons/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/superperoncho/icons/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()],
+  base: '/superperoncho/'
 })
+
 
 
