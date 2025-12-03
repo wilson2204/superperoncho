4dd8bed5d079f94ea3b73e4f0027ebbe65d5aa8c
Agustin Wilson <aguswilson2015@outlook.es>
2025-12-03 16:56:11 -0300

    Guardar cambios antes del deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/superperoncho/'
-})
-
-
-
+  base: "/superperoncho/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Super Peroncho",
+        short_name: "Peroncho",
+        start_url: "/superperoncho/",
+        scope: "/superperoncho/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
